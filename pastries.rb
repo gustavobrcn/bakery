b96fb18b13a7ffebef09bafd04a5fd8e88fbdae0
gustavo <gustavobrcn@hotmial.com>
2019-12-11 16:16:16 -0500

    created classes and some background styling

---
+++ pastries.rb
@@ -0,0 +1,20 @@
+class Cookies
+    initialize(name, price)
+    @name = name
+    @price = price
+    @discription = 
+end
+
+class Cake
+    initialize(name, price)
+    @name = name
+    @price = price
+    @discription
+end
+
+class Muffin
+    initialize(name, price)
+    @name = name
+    @price = price
+    @discription
+end
