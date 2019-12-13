ee1a2794c11171f16f44a6a0a1d3b414dd7cf4fe
gustavo <gustavobrcn@hotmial.com>
2019-12-12 23:35:55 -0500

    Added small title with animation and javascript

--- public/main.js
+++ public/main.js
@@ -1,12 +1,19 @@
 let title = document.getElementById('title')
+let smallTitle = document.getElementById('small-title')
 
 const titleAppear = () => {
     title.style.display = 'block'
-} 
+}
 
 const titleDisappear = () => {
     title.style.display = 'none'
 }
 
-setTimeout(titleAppear, 1500)
+setTimeout(titleAppear, 500)
 setTimeout(titleDisappear, 6000)
+
+const smallTitleAppear = () => {
+    smallTitle.style.display = 'block'
+}
+
+setTimeout(smallTitleAppear, 5000)
