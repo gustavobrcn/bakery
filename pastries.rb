a8bc820d6a37226cde9d7962c35ab8a76ebad490
gustavo <gustavobrcn@hotmial.com>
2019-12-14 18:41:29 -0500

    finished the cookies class. Added case switch for images and description. Made a file with hashes but the data doesnt render on the page

--- pastries.rb
+++ pastries.rb
@@ -1,28 +1,66 @@
 class Cookies
-    attr_accessor :name, :description
+    attr_accessor :name, :description, :image
     attr_reader :price
-    def initialize(name)
+    def initialize(name, description, image)
         @name = name
-        @price = '$1B'
-        @description = 'This treat is not for those of weak constitution or the poor. If you are not prepared for the awsome taste of this Godly treat you will be crushed under the weight of its glory!'
+        @price = '$1M'
+        @description = nil
+        @image = nil
+        self.show_image(image)
+        self.show_description(description)
     end
+
+    def show_image(image)
+        @image = nil
+        case image
+        when 1
+            @image = '/images/cookies/cookie1.png'
+        when 2
+            @image = '/images/cookies/cookie2.png'
+        when 3
+            @image = '/images/cookies/cookie3.png'
+        else
+            @description = 'Invalid input'
+        end
+    end
+
+    def show_description(description)
+        @description = nil
+        case description
+        when 1
+            @description = 'A classic combo of buttery oats & plump raisins with a spicy sweetness that will keep you coming back for more.'
+        when 2
+            @description = 'This hometown favorite is chock full of chocolate chunks (say that three times fast), toasted walnuts, oats & coconut.'
+        when 3
+            @description = 'Made with shredded coconut, sugar, eggs & oat flour. Hand-dipped in chocolate for a decadent finishing touch. '
+        else
+            @description = 'Input valid number'
+        end
+    end
+
 end
 
-class Cake
-    attr_accessor :name, :description
-    attr_reader :price
-    def initialize(name, price)
-    @name = name
-    @price = price
-    @discription
-    end
-end
-
-# class Muffin
-#     initialize(name, price)
-#     @name = name
-#     @price = price
-#     @discription
+# class Muffins
+#     attr_accessor :name, :description
+#     attr_reader :price
+#     def initialize(name, description)
+#         @name = name
+#         @price = '$1.5M'
+#         @description = description
+#     end
 # end
 
-# @god_cookies = Cookies.new('God Cookie', '$1B')
+# class Cakes
+#     attr_accessor :name, :description
+#     attr_reader :price
+#     def initialize(name, description)
+#         @name = name
+#         @price = '$1B'
+#         @description = description
+#     end
+# end
+
+# @name = @cookies[1]['name']
+# @cookie1 = Cookies.new(@name, 1)
+
+
--- server.rb
+++ server.rb
@@ -1,11 +1,10 @@
 require 'sinatra'
 require 'httparty'
 require './pastries'
+require './bakery-data'
 
-get '/' do
-    @god_cookies = Cookies.new('God Cookie')
-    puts @god_cookies  
+
+get '/' do  
+    @cookie1 = Cookies.new("Godly oatmeal", 1, 1)
     erb  :index
 end
-    
-
