cc72c408683777e6143f11e6f3da37ce4e4e0fdd
gustavo <gustavobrcn@hotmial.com>
2019-12-18 08:59:18 -0500

    Removed numbers from the cookies array

--- bakery-data.rb
+++ bakery-data.rb
@@ -1,17 +1,11 @@
-@cookies = [
-    1 => {
+$cookies = [
+    {
         :name => 'Godly Oatmeal Cookie',
         :description => 'A classic combo of buttery oats & plump raisins with a spicy sweetness that will keep you coming back for more.'
 },
-    2 => {
+    {
         :name => 'Godly Chocalate Cookie',
         :description => 'This hometown favorite is chock full of chocolate chunks (say that three times fast), toasted walnuts, oats & coconut.'
     }
 ]
    
-def name(index, cookie)
-   @name = @cookies[index][cookie][:name]
-    return @name
-end
-
-# puts name(0, 1)
